01cbebe099f7413a25447141c5f72212eb26a1f8
bastien <bastien.raoult1pro@gmail.com>
2023-04-03 16:11:35 +0200

    update

--- www/navbar.php
+++ www/navbar.php
@@ -1,45 +1,32 @@
-<?php
-
-// On récupère le nom du fichier php
-$filename = basename($_SERVER['REQUEST_URI']);
-
-$informationPesonnellesActive = '';
-$projetsActive = '';
-$connectionActive = '';
-
-if (strpos($filename, 'informationPersonnelles') !== false) {
-    $devoirActive = '';
-    $connectionActive = '';
-    $informationPesonnellesActive = 'active';
-} else if (strpos($filename, 'utilisateur') !== false) {
-    $informationPesonnellesActive = '';
-    $connectionActive = '';
-    $utilisateurActive = 'active';
-} else if (strpos($filename, 'projets') !== false) {
-    $informationPesonnellesActive = '';
-    $connectionActive = '';
-    $projetsActive = 'active';
-} else if (strpos($filename, 'connection') !== false) {
-    $informationPesonnellesActive = '';
-    $projetsActive = '';
-    $connectionActive = 'active';
-}
-?>
 <link rel="stylesheet" href="../css/nav.css">
 
-<ul class="nav">
-    <a href="index.php"><img class="perso"src="img/2.png" alt></a>
-    <li class="nav-item2">
-        <a class="nav-link <?php echo $informationPesonnellesActive ?>" href="informationPersonnelles.php">Informations Personnelles</a>
-    </li>
+<nav class="navbar">
+    <a href="index.php"><img class="perso" src="img/2.png" alt></a>
+    <div class="nav-links">
+        <ul>
+            <li>
+                <a href="informationPersonnelles.php">Informations Personnelles</a>
+            </li>
+            <li>
+                <a href="#">Projets</a>
+            </li>
+            <li>
+                <a href="cv.php">CV</a>
+            </li>
+            <li>
+                <a href="login.php">Connection</a>
+            </li>
+        </ul>
+    </div>
+    <img src="img/test.png" class="mob-menu">
+<body>
+    <script>
+        const mobMenu = document.querySelector("mob-menu")
+        const navLinks = document.querySelector("nav-links")
 
-    <li class="nav-item3">
-        <a class="nav-link <?php echo $projetsActive ?>" href="#">Projets</a>
-    </li>
-    <li class="nav-item3">
-        <a class="nav-link <?php echo $projetsActive ?>" href="cv.php">CV</a>
-    </li>
-    <li class="nav-item4">
-        <a class="nav-link <?php echo $connectionActive ?>" href="login.php">Connection</a>
-    </li>
-</ul>
+        mobMenu.addEventListener('click',()=>{
+            navLinks.toggle('mobile-menu')
+        })
+    </script>
+</body>
+</nav>
