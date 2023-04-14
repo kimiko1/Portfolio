a88569513bc2c347d3ffe0b6be0b25c953bea3d9
bastien <bastien.raoult1pro@gmail.com>
2023-04-14 16:55:24 +0200

    update

--- public_html/informationPersonnelles.php
+++ public_html/informationPersonnelles.php
@@ -10,6 +10,9 @@ $infos = $bdd->recupInfo();
 <html>
 
 <head>
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Information Personnelles</title>
     <?php require('navbar.php'); ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1
