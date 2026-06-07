fcc00ba8ffd8d3fbce2d94dc2d7770103c8aec79
kimiko1 <bastien.raoult1pro@gmail.com>
2026-06-07 18:05:28 +0200

    ✨ Initial commit

---
+++ app/(User)/page.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">User Home Page</h1>
+    </div>
+  );
+}
---
+++ app/providers.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Toaster } from "@/components/ui/sonner";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider refetchOnWindowFocus={false} refetchInterval={0}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+        <Toaster richColors position="top-right" />
+      </ThemeProvider>
+    </SessionProvider>
+  );
+}
